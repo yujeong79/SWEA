e92fa86bab5eb644ae1bf0397b97e0907ecb2aee
김유정 <aabbc0908@naver.com>
2024-08-27 11:43:16 +0900

    2024-08-27(11:43)

--- SWEA/src/구간합_1차원배열.java
+++ SWEA/src/구간합_1차원배열.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 구간합 조건 : 합을 구할 배열의 원소가 바뀌지 않는 경우
  * 구간합 구하는 시간 : O[N]
@@ -7,10 +9,15 @@
 public class 구간합_1차원배열 {
 	public static void main(String[] args) {
 		int[] map = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-		int[] prefixSum = new int[map.length];
-		
+
 		for(int i = 0; i < map.length; i++) {
-			prefixSum[i] = prefixSum[i-1] + map[i];
+			map[i] = map[i-1] + map[i];
 		}
+		
+		System.out.println(Arrays.toString(map));
+		
+		//  구간합 : i(포함) ~ j(미포함), 3~8
+		System.out.println(map[8-1] - map[3]);
+		
 	} // end of main
 } // end of class
