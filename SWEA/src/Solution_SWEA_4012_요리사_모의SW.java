7c03cb4dc5de8897632668ae5fc384c572f770e2
김유정 <aabbc0908@naver.com>
2024-08-27 17:55:38 +0900

    commit

--- SWEA/src/Solution_SWEA_4012_요리사_모의SW.java
+++ SWEA/src/Solution_SWEA_4012_요리사_모의SW.java
@@ -1,17 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * N개의 식재료를 절반씩 나누어 A와 B 두 개의 요리를 하려고 한다.
- * A음식과 B음식의 맛의 차이가 최소가 되도록 재료를 배분해야 한다.
- * 
- * 식재료1과 식재료2 => A음식, 식재료3과 식재료4 => B음식 : 차이 3
- * 식재료2와 식재료4 => A음식, 식재료1과 식재료3 => B음식 : 차이 2
- * 
- * 정답 : 2
- * 
- */
-
 public class Solution_SWEA_4012_요리사_모의SW {
 	static int N;
 	static int R;
@@ -95,31 +84,23 @@ public class Solution_SWEA_4012_요리사_모의SW {
 					int i1 = listA.get(i);
 					int i2 = listA.get(j);
 					
-					//System.out.println("식재료1 : " + i1 + ", 식재료2 : " + i2 + ", 시너지 : " + synergy[i1][i2]);
 					foodA += synergy[i1][i2];
 				}
 			}
 		}
 		
-		for(int i = 0; i < listB.size(); i++) { // foodA 시너지
+		for(int i = 0; i < listB.size(); i++) { // foodB 시너지
 			for(int j = 0; j < listB.size(); j++) {
 				if(listB.get(i) != listB.get(j)) { // 서로 다른 식재료인 경우에 식재료의 시너지 구하기
 					int i1 = listB.get(i);
 					int i2 = listB.get(j);
 					
-					//System.out.println("식재료1 : " + i1 + ", 식재료2 : " + i2 + ", 시너지 : " + synergy[i1][i2]);
 					foodB += synergy[i1][i2];
 				}
 			}
 		}
 		
 		min = Math.min(min, Math.abs(foodA - foodB));
-		
-		//System.out.println(foodA);
-		//System.out.println(foodB);
-		//System.out.println("Diff : " + Math.abs(foodA - foodB));
-		
-		
 	}
 	
 } // end of class
