d40a8e557be9743b9cc332169f79c2961dba1e81
김유정 <aabbc0908@naver.com>
2024-09-03 15:42:00 +0900

    commit

--- SWEA/src/Solution_SWEA14510_나무높이_D2_김유정.java
+++ SWEA/src/Solution_SWEA14510_나무높이_D2_김유정.java
@@ -2,15 +2,8 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 하루에 한 나무에만 물을 줄 수 있다.
- * 홀수 날은 1씩 자라고 짝수 날은 2씩 자란다.
- * 모든 나무의 키가 처음에 가장 키가 컸던 나무와 같아지도록 할 수 있는 최소 날짜 수
+ * 그리디
  * 
- * 예시
- * 1
-20
-26 19 23 2 24 2 17 15 1 27 6 29 18 23 27 13 26 21 9 1
-[28, 28, 27, 27, 23, 20, 16, 14, 12, 11, 10, 8, 6, 6, 5, 3, 3, 2, 2, 0]
  */
 public class Solution_SWEA14510_나무높이_D2_김유정 {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -54,8 +47,9 @@ public class Solution_SWEA14510_나무높이_D2_김유정 {
 		if(t >= N-1) return;
 		
 		while(trees[t] > 0) {
-			//System.out.println(day + ", " + Arrays.toString(trees));
+			System.out.println(day + ", " + Arrays.toString(trees));
 			day++;
+			System.out.println(day + ", " + Arrays.toString(trees));
 			int growth = day%2!=0? 1 : 2;
 			
 			if(trees[t] == 2 && growth == 1) {
