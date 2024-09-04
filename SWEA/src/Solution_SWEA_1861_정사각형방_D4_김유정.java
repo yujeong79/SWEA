7748bd855d3e14b41fab5bb923fcc3d6c7bec625
김유정 <aabbc0908@naver.com>
2024-09-04 17:23:30 +0900

    commit

--- SWEA/src/Solution_SWEA_1861_정사각형방_D4_김유정.java
+++ SWEA/src/Solution_SWEA_1861_정사각형방_D4_김유정.java
@@ -33,12 +33,17 @@ public class Solution_SWEA_1861_정사각형방_D4_김유정 {
 			for(int i = 1; i <= N; i++) {
 				for(int j = 1; j <= N; j++) {
 					BFS(i,j);
-					startRoom[origin[i][j]] = cnt; 
-					max = Math.max(max, cnt);
+					max = Math.max(max, startRoom[origin[i][j]]);
 				}
 			}
 			
-			sb.append("#" + testCase + " " + false + " " + max);
+			sb.append("#" + testCase + " ");
+			for(int i = 1; i <= N*N; i++) {
+				if(startRoom[i] == max) {
+					sb.append(i + " " + max + "\n");
+					break;
+				}
+			}
 			
 			System.out.println(Arrays.toString(startRoom));
 			
@@ -46,11 +51,10 @@ public class Solution_SWEA_1861_정사각형방_D4_김유정 {
 		System.out.println(sb);
 	} // end of main
 	
-	private static int cnt;
-	
 	private static void BFS(int i, int j) {
 		int[] start = {i, j};
 		Queue<int[]> queue = new LinkedList<>();
+		startRoom[origin[i][j]]++;
 		
 		for(int k = 1; k <= N; k++) {
 			for(int l = 1; l <= N; l++) {
@@ -60,19 +64,20 @@ public class Solution_SWEA_1861_정사각형방_D4_김유정 {
 		
 		queue.add(start);
 		rooms[i][j] = 0;
-		cnt = 1;
 		
 		while(!queue.isEmpty()) {
 			int[] curr = queue.poll();
 			int r = curr[0];
 			int c = curr[1];
+			System.out.println(rooms[r][c]);
 			
 			if(rooms[r+dr[0]][c+dc[0]] != 0 || rooms[r+dr[1]][c+dc[1]] != 0 || rooms[r+dr[2]][c+dc[2]] != 0 || rooms[r+dr[3]][c+dc[3]] != 0) {
 				for(int d = 0; d < 4; d++) {
 					if(rooms[r+dr[d]][c+dc[d]] != 0 && rooms[r+dr[d]][c+dc[d]] - rooms[r][c] == 1) {
-						queue.add(new int[] {r+dr[d], c+dc[d]});
+						startRoom[origin[i][j]]++;
+						int[] next = {r+dr[d], c+dc[d]};
+						queue.add(next);
 						rooms[r+dr[d]][c+dc[d]] = 0;
-						cnt++;
 					}
 				}
 			}
