e8f61709180a7589b99e560d18c15ccaae2d5aef
김유정 <aabbc0908@naver.com>
2024-09-06 14:14:56 +0900

    commit

---
+++ SWEA/src/Solution_SWEA_2382_미생물격리_모의SW_김유정.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class Solution_SWEA_2382_미생물격리_모의SW_김유정 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
