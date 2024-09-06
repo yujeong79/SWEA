87728f55bbe7db1acf22698587496b9637248134
김유정 <aabbc0908@naver.com>
2024-09-06 14:12:50 +0900

    commit

---
+++ SWEA/src/Solution_SWEA_2112_보호필름_모의SW.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution_SWEA_2112_보호필름_모의SW {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		int testCase = 0;
+		while(++testCase <= T) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int D = Integer.parseInt(st.nextToken());
+			int W = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+		} // end of testCase
+	} // end of main
+} // end of class
