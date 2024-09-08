05c6988750434d5afc4ff3994d8fce0d56e20666
yujeong <yujeong@localhost>
2024-09-08 13:10:36 +0900

    commit

--- SWEA/src/Solution_SWEA_1952_수영장_모의SW.java
+++ SWEA/src/Solution_SWEA_1952_수영장_모의SW.java
@@ -1,11 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 각 이용권의 요금과 각 달의 이용 계획이 입력
- * 가장 적은 비용으로 수영장을 이용할 수 있는 방법을 찾고 그 비용을 정답으로 출력
- */
-public class Solution_SWEA_1952_수영장_모의SW {
+public class Solution_SWEA_1952_수영장_모의SW {	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -14,18 +10,23 @@ public class Solution_SWEA_1952_수영장_모의SW {
 		int testCase = 0;
 		while(++testCase <= T) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int[] tickets = new int[4];
+			int[] cost = new int[4];
 			for(int i = 0; i < 4; i++) {
-				tickets[i] = Integer.parseInt(st.nextToken());
+				cost[i] = Integer.parseInt(st.nextToken());
 			}
 			
 			st = new StringTokenizer(br.readLine(), " ");
-			int[] plan = new int[12];
+			int[] month = new int[12];
 			for(int i = 0; i < 12; i++) {
-				plan[i] = Integer.parseInt(st.nextToken());
+				month[i] = Integer.parseInt(st.nextToken());
 			}
 			
+			int min = Integer.MAX_VALUE;
 			
-		} // end of testCase
+			
+
+			sb.append("#" + testCase + " " + min + "\n");
+		} // end of testCase	
+		System.out.println(sb);
 	} // end of main
 } // end of class
