f83250241579c8c9ed28fb58c4bf44cbf10dd572
김유정 <aabbc0908@naver.com>
2024-09-12 17:10:42 +0900

    commit

--- SWEA/src/Solution_SWEA_1949_등산로조성_모의SW.java
+++ SWEA/src/Solution_SWEA_1949_등산로조성_모의SW.java
@@ -2,16 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Solution_SWEA_1949_등산로조성_모의SW {
-	private static int[] dr = {-1, 1, 0, 0};
-	private static int[] dc = {0, 0, -1, 1};
-	
-	private static int N;
-	private static int K;
-	private static int[][] map;
-	private static int[][] tmp;
-	private static boolean[][] isVisited;
-	private static int length;
+	static class Point {
+		int r, c, l;
 
+		public Point(int r, int c, int l) {
+			this.r = r;
+			this.c = c;
+			this.l = l;
+		}
+
+		@Override
+		public String toString() {
+			return "(" + r + ", " + c + ")의 length : " + l;
+		}
+	}
+	
+	static class Construction {
+		int r, c, length;
+		boolean state;
+		
+		public Construction() {
+			this.state = false;
+		}
+		
+		public Construction(int r, int c, boolean state, int length) {
+			this.r = r;
+			this.c = c;
+			this.state = state;
+			this.length = length;
+		}
+	}
+	
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	static int N, K;
+	static int[][] map;
+	
+	static boolean[][] isVisited;
+	static int maxLength;
+	
+	static Construction check;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -19,85 +51,62 @@ public class Solution_SWEA_1949_등산로조성_모의SW {
 		int T = Integer.parseInt(br.readLine());
 		int testCase = 0;
 		while(++testCase <= T) {
-			int answer = 0; // 각 테스트케이스의 답
-			
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			N = Integer.parseInt(st.nextToken()); // 지도 한 변의 길이, N*N
 			K = Integer.parseInt(st.nextToken()); // 최대 공사 가능 깊이
 			
-			int max = 0; // 봉우리
+			int highestPoint = 0; // 봉우리
 			map = new int[N][N];
 			for(int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
 				for(int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					max = Math.max(max, map[i][j]);
+					highestPoint = Math.max(highestPoint, map[i][j]);
 				}
 			}
 			
-			tmp = new int[N][N];
+			maxLength = 0;
 			for(int i = 0; i < N; i++) {
 				for(int j = 0; j < N; j++) {
-					if(map[i][j] == max) {
-						for(int ii = 0; ii < N; ii++) {
-							for(int jj = 0; jj < N; jj++) {
-								tmp[ii][jj] = map[ii][jj];
-							}
-						}
-						length = 1;
-						//flag = false;
-						isVisited = new boolean[N][N];
-						BFS(i, j); // 모든 등산로는 봉우리에서 시작
-						answer = Math.max(length, answer);
+					if(map[i][j] == highestPoint) {
+						check = new Construction();
+						DFS(i, j, 0);
 					}
 				}
 			}
-			sb.append("#" + testCase + " " + answer + "\n");
+
+			sb.append("#" + testCase + " " + maxLength + "\n");
 		} // end of testCase
 		System.out.println(sb);
 	} // end of main
 
-	private static void BFS(int i, int j) {
-		Queue<int[]> queue = new LinkedList<>();
+	private static void DFS(int i, int j, int roadLength) {
+		Stack<Point> stack = new Stack<>();
 		
-
-		int[] point = {i, j, 1, 0};
-		queue.add(point);
-		isVisited[i][j] = true;
+		stack.add(new Point(i, j, 1));
 		
-		while(!queue.isEmpty()) {
-			int[] curr = queue.poll();
-			int r = curr[0];
-			int c = curr[1];
-			int l = curr[2];
-			int construction = curr[3]; // 0이면 공사를 아직 하지 않음, 1이면 공사를 했음
-			length = l;
-			isVisited[r][c] = true;
+		int[][] tempMap = new int[N][N];
+		for(int r = 0; r < N; r++) {
+			tempMap[r] = map[r].clone();
+		}
+		
+		while(!stack.isEmpty()) {
+			Point curr = stack.pop();
+			maxLength = Math.max(curr.l, maxLength);
 			
-			System.out.println(r +", " +c);
-			for(int[] a : tmp) {
-				System.out.println(Arrays.toString(a));
-			}
-			System.out.println("length : " + length);
-			System.out.println();
-			
-			for(int d = 0; d < 4; d++) { // 상 하 좌 우 탐색
-				if(r+dr[d] >= 0 && r+dr[d] < N && c+dc[d] >= 0 && c+dc[d] < N && !isVisited[r+dr[d]][c+dc[d]]) { // 범위 안에 있는지
-					if(tmp[r+dr[d]][c+dc[d]] < tmp[r][c]) { // 현재 위치보다 작으면
-						point = new int[] {(r+dr[d]), (c+dc[d]), l+1, construction};
-						queue.add(point);
-					} else {
-						for(int k = 1; construction == 0 && k <= K; k++) {
-							if(tmp[r+dr[d]][c+dc[d]] - k < tmp[r][c]) {
-								tmp[r+dr[d]][c+dc[d]] -= k;
-								point = new int[] {(r+dr[d]), (c+dc[d]), l+1, construction*-1};
-								queue.add(point);
-							}
-						}
+			for(int d = 0; d < 4; d++) {
+				int r = curr.r + dr[d];
+				int c = curr.c + dc[d];
+				if(r >= 0 && r < N && c >= 0 && c < N) {
+					if(tempMap[curr.r][curr.c] > tempMap[r][c]) {
+						stack.add(new Point(r, c, curr.l+1));
+					} else if(!check.state && tempMap[curr.r][curr.c] > tempMap[r][c]-K) {
+						check = new Construction(r, c, true, curr.l+1);
+						stack.add(new Point(r, c, curr.l+1));
+						tempMap[r][c] = tempMap[curr.r][curr.c]-1;
 					}
 				}
 			}
-			
 		}
 	}
 	
